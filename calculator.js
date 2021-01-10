6b261fbbc25e7ff85c5692f9d7bc6a9124c9e4dc
devolisto <devolisto@gmail.com>
2021-01-10 15:42:47 +0300

    conditionals task done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
 // Define the numbers and the operation here
+let firstNumber = 5;
+let secondNumber = 10;
+let theOperation = "-";
 
 // Write if-statements here
+// I think this if statment could be written better i.e. the final abroad else statemnt 
+if(theOperation === "+") {
+    console.log(firstNumber + secondNumber);
+} else if (theOperation === "-") {
+    console.log(firstNumber - secondNumber);
+} else {
+    console.log(firstNumber / secondNumber);
+}
+
+
+
+// n calculator.js, create an integer variable for the first number.
+// Create a second integer variable for the second number.
+// Create a third string variable for the operation (addition "+", subtraction "-", multiplication "*", or division "/").
+// Use if-statements to check which operation the third variable has.
+// Perform the mathematical operation on the two numbers, and print the result to the console.
